022f72ce144d697179c5386465824ab49bee58ae
govindaa8709 <83050872+govindaa8709@users.noreply.github.com>
2023-07-01 11:42:01 +0545

    Add files via upload

---
+++ app/admin.py
@@ -0,0 +1,116 @@
+from django.db import models
+from django.contrib import admin
+from django.db.models.signals import pre_delete, pre_save
+from django.dispatch.dispatcher import receiver
+from app.models import Category, Message, Product, ProductImages, Bid, Rating, Seller, UserDetails, Payment
+import os
+from tinymce.widgets import TinyMCE
+from tinymce.models import HTMLField 
+from import_export.admin import ImportExportModelAdmin
+
+
+
+
+
+class ProductAdmin(ImportExportModelAdmin):
+    list_display = ('id','title', 'admin_photo','actualAmount','category','post_view')
+    #filter product by category and starting time
+    list_filter = ('category','startingTime')
+    list_per_page = 20
+    description =  HTMLField()
+    
+    formfield_overrides = {
+    models.TextField: {'widget': TinyMCE()}
+    }
+   
+    
+    #delete image from folder when deleting post
+    @receiver(pre_delete, sender=Product)
+    def post_save_image(sender, instance, *args, **kwargs):
+        try:
+            instance.image.delete(save=False)
+        except:
+            pass
+        
+    # update image in the folder while updating the post
+    @receiver(pre_save, sender=Product)
+    def pre_save_image(sender, instance, *args, **kwargs):
+        try:
+            old_img = instance.__class__.objects.get(id=instance.id).image.path
+            try:
+                new_img = instance.image.path
+            except:
+                new_img = None
+            if new_img != old_img:
+                if os.path.exists(old_img):
+                    os.remove(old_img)
+        except:
+            pass
+
+class ProductImagesAdmin(admin.ModelAdmin):
+    list_display = ('product', 'admin_images')
+    #pagination with 15 item in one page
+    list_per_page = 15
+    
+    #delete image from folder when deleting post
+    @receiver(pre_delete, sender=ProductImages)
+    def post_save_image(sender, instance, *args, **kwargs):
+        try:
+            instance.images.delete(save=False)
+        except:
+            pass
+    # update image in the folder while updating the post  
+    @receiver(pre_save, sender=ProductImages)
+    def pre_save_image(sender, instance, *args, **kwargs):
+        try:
+            old_img = instance.__class__.objects.get(id=instance.id).images.path
+            try:
+                new_img = instance.images.path
+            except:
+                new_img = None
+            if new_img != old_img:
+                if os.path.exists(old_img):
+                    os.remove(old_img)
+        except:
+            pass
+    
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('product', 'user','bidPrice','time','status','payment')
+    search_fields = ['product__title']
+    list_per_page = 20
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'description')
+
+class UserDetailsAdmin(admin.ModelAdmin):
+    list_display = ['user','phone','address']
+    
+    #delete image from folder when deleting post
+    @receiver(pre_delete, sender=UserDetails)
+    def post_save_image(sender, instance, *args, **kwargs):
+        try:
+            instance.profilePicture.delete(save=False)
+        except:
+            pass
+        
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('paymentId','product', 'user','amount','token')   
+    
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('fullname','email','message')
+
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('user','fullname','description')
+    
+class RatingAdmin(ImportExportModelAdmin):
+    list_display = ('user','product','rate')
+
+admin.site.register(Rating,RatingAdmin)
+admin.site.register(Seller, SellerAdmin)
+admin.site.register(Message,MessageAdmin)
+admin.site.register(Payment,PaymentAdmin)    
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
+admin.site.register(ProductImages,ProductImagesAdmin)
+admin.site.register(Bid,BidAdmin)
+admin.site.register(UserDetails, UserDetailsAdmin)
---
+++ demo/info.py
@@ -0,0 +1,6 @@
+# from decouple import config
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'hamroauctionsystem@gmail.com'
+EMAIL_HOST_PASSWORD ='testpassword'
+EMAIL_PORT = 587
